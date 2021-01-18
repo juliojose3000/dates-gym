acd7bed6e8a9e95ade0dab89cca0bb6876764c6f
Julio Segura <jsegurac@tecnosys.net>
2021-01-18 12:01:01 -0600

    #GymDates -> Develop: heroku envonment added

--- src/environments/environment.heroku.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  url: "https://gym-dates-backend.herokuapp.com/gymcachi"
-};
-
-
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 var localhost = "http://localhost:8080/gymcachi";
 var azure = "https://gymdates.azurewebsites.net/gymcachi";
+var heroku = "https://gym-dates-backend.herokuapp.com/gymcachi";
 
 export const environment = {
   production: true,
-  url: azure
+  url: heroku
 };
 
 
